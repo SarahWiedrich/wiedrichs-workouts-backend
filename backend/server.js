21c90e5374c387b7c348d079d839d0c511c0897f
esziemba <97561230+esziemba@users.noreply.github.com>
2022-07-23 14:05:29 -0500

    added routes

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,15 @@
 require('dotenv').config()
 
+const { response } = require('express')
 const express = require('express')
 
 // express app
 const app = express()
+
+// routes
+app.get('/', (req, res) => {
+    res.send('Welcome to the app!')
+})
 
 // listen for requests
 app.listen(process.env.PORT, () => {
