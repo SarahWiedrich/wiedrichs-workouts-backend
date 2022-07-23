f17e4d52dc24b22ce7041e78fe4a484e416f1cd6
esziemba <97561230+esziemba@users.noreply.github.com>
2022-07-23 17:12:33 -0500

    added new middleware

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,8 @@ const workoutRoutes = require('./routes/workouts')
 const app = express()
 
 // middleware
+app.use(express.json())
+
 app.use((req, res, next) => {
     console.log(req.path, req.method)
     next()
