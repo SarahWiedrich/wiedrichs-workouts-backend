1e0fc765c2bb968dd5b31d570ec214d5a96ad769
Sarah Wiedrich <97638277+SarahWiedrich@users.noreply.github.com>
2022-07-26 21:25:59 -0500

    default images added to both schemas

--- models/recipeModel.js
+++ models/recipeModel.js
@@ -13,6 +13,7 @@ const recipeSchema = new Schema({
     },
     imgURL: {
         type: String,
+        default: 'https://images.unsplash.com/photo-1567620832903-9fc6debc209f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y2hpY2tlbiUyMHdpbmdzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60'
     },
     serving: {
         type: Number,
--- models/workoutModel.js
+++ models/workoutModel.js
@@ -11,7 +11,8 @@ const workoutSchema = new Schema({
         type: String
     },
     imgURL: {
-        type: String
+        type: String,
+        default: "https://images.unsplash.com/photo-1571019613454-1cb2f99b2d8b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8Zml0bmVzc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
     },
     type: {
         type: String
