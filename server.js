9eee458e72b5bf21741e7375a9260d2b4864b237
Sarah Wiedrich <97638277+SarahWiedrich@users.noreply.github.com>
2022-07-26 19:08:44 -0500

    updated recipe route in server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/workouts')
+const recipeRoutes = require('./routes/workouts')
 
 // express app
 const app = express()
@@ -16,6 +17,7 @@ app.use((req, res, next) => {
 
 // routes
 app.use('/api/workouts', workoutRoutes)
+app.use('/api/recipes', recipeRoutes)
 
 // connect to mongoose
 mongoose.connect(process.env.MONGO_URI)
