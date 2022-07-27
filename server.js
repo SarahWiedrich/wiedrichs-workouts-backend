1165dff80722af8210d75dd3a865ec027c3cafc8
Sarah Wiedrich <97638277+SarahWiedrich@users.noreply.github.com>
2022-07-26 19:46:22 -0500

    installed cors

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require('cors')
 const workoutRoutes = require('./routes/workouts')
 const recipeRoutes = require('./routes/recipes')
 
@@ -9,6 +10,7 @@ const app = express()
 
 // middleware
 app.use(express.json())
+app.use(cors())
 
 app.use((req, res, next) => {
     console.log(req.path, req.method)
