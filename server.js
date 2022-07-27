37efcd815ca6360739d27ebcbd9c5702783d98f5
Sarah Wiedrich <97638277+SarahWiedrich@users.noreply.github.com>
2022-07-26 19:09:53 -0500

    updated recipe routes in server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/workouts')
-const recipeRoutes = require('./routes/workouts')
+const recipeRoutes = require('./routes/recipes')
 
 // express app
 const app = express()
